52042b173a380f2e00131e4dda01279e497752a9
ADItyaP999 <adityapujari2@gmail.com>
2019-06-15 10:52:12 +0530

    Initial Commit

---
+++ Assets/CourseMaterials/Scripts/DragonControls.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class DragonControls : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Transform Dragon;
+
+    public float Speed =1.5f;
+    public float rotationSpeed=75f;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        float translation=CrossPlatformInputManager.GetAxis("Vertical") * Speed;
+        float rotation=CrossPlatformInputManager.GetAxis("Horizontal") * rotationSpeed;
+        translation*=Time.deltaTime;
+        rotation*=Time.deltaTime;
+        Dragon.Translate(0,0,translation);
+        Dragon.Rotate(0,rotation,0);
+    }
+}
